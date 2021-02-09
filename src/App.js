ad308947f82e843334197fb459f38bd2d2218764
Aman <amanueltek19@gmail.com>
2021-02-09 14:56:51 -0500

    initial commit to my navbar

---
+++ src/App.js
@@ -0,0 +1,19 @@
+/** @format */
+import React from 'react';
+
+// components
+
+import './App.css';
+import MyNavbar from './components/Navbar/MyNavbar';
+
+const App = () => {
+	return (
+		<div>
+		
+			<MyNavbar />
+      My Portfolio
+		</div>
+	);
+};
+
+export default App;
---
+++ src/themes/theme.js
@@ -0,0 +1,6 @@
+/** @format */
+
+export const theme = {
+	primary: '#0b0c10',
+	textColor: '#66fcf1',
+};
