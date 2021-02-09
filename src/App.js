b812274168ab0c660196764e6a08be93d690e0fa
Aman <amanueltek19@gmail.com>
2021-02-09 16:44:08 -0500

    Added carousal and scrolldown components

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import React from 'react';
 // components
 
 import './App.css';
+import MyCarousal from './components/my-carousal/my-carousal';
 import MyNavbar from './components/Navbar/MyNavbar';
+import TitleMessage from './components/title-message/title-message';
 
 const App = () => {
 	return (
 		<div>
-		
 			<MyNavbar />
-      My Portfolio
+      <MyCarousal />
+      <TitleMessage/>
 		</div>
 	);
 };
