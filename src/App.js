f08fc970f5e226a13960da224cb68d838d8baa1a
Aman <amanueltek19@gmail.com>
2021-02-09 23:08:11 -0500

    Added about page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 /** @format */
 import React from 'react';
+import Fade from 'react-reveal/Fade'
+import { Parallax } from "react-parallax"
+import Container from 'react-bootstrap/Container'
 
 // components
 
@@ -7,14 +10,32 @@ import './App.css';
 import MyCarousal from './components/my-carousal/my-carousal';
 import MyNavbar from './components/Navbar/MyNavbar';
 import TitleMessage from './components/title-message/title-message';
+import About from './pages/about/about';
 
 const App = () => {
 	return (
-		<div>
+		<div className="App" style={{ position: "relative" }}>
 			<MyNavbar />
       <MyCarousal />
       <TitleMessage/>
-		</div>
+      <div>
+        <Parallax
+          blur={{ min: -30, max: 30 }}
+          bgImage={require("./assets/img/parallex/background.webp")}
+          bgImageAlt=""
+          strength={-200}
+        >
+          <div>
+            <Container className="container-box rounded">
+              <Fade duration={500}>
+                <About />
+              </Fade>
+            </Container>
+          </div>
+        </Parallax>
+      </div>
+      </div>
+
 	);
 };
 
