e3bbb9f6a3b4d5a4967608d914c77c92fffafb3b
Aman <amanueltek19@gmail.com>
2021-02-10 00:54:32 -0500

    Added skill page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 /** @format */
 import React from 'react';
-import Fade from 'react-reveal/Fade'
-import { Parallax } from "react-parallax"
-import Container from 'react-bootstrap/Container'
+import Fade from 'react-reveal/Fade';
+import { Parallax } from 'react-parallax';
+import Container from 'react-bootstrap/Container';
+import Slide from 'react-reveal/Slide';
 
 // components
 
@@ -11,31 +12,38 @@ import MyCarousal from './components/my-carousal/my-carousal';
 import MyNavbar from './components/Navbar/MyNavbar';
 import TitleMessage from './components/title-message/title-message';
 import About from './pages/about/about';
+import Skills from './pages/skills/skills';
 
 const App = () => {
 	return (
-		<div className="App" style={{ position: "relative" }}>
+		<div className='App' style={{ position: 'relative' }}>
 			<MyNavbar />
-      <MyCarousal />
-      <TitleMessage/>
-      <div>
-        <Parallax
-          blur={{ min: -30, max: 30 }}
-          bgImage={require("./assets/img/parallex/background.webp")}
-          bgImageAlt=""
-          strength={-200}
-        >
-          <div>
-            <Container className="container-box rounded">
-              <Fade duration={500}>
-                <About />
-              </Fade>
-            </Container>
-          </div>
-        </Parallax>
-      </div>
-      </div>
-
+			<MyCarousal />
+			<TitleMessage />
+			<div>
+				<Parallax
+					blur={{ min: -30, max: 30 }}
+					bgImage={require('./assets/img/parallex/background.webp')}
+					bgImageAlt=''
+					strength={-200}>
+					<div>
+						<Container className='container-box rounded'>
+							<Fade duration={500}>
+								<About />
+							</Fade>
+						</Container>
+					</div>
+				</Parallax>
+			</div>
+			<div>
+				<Container className='container-box rounded'>
+					<Slide bottom duration={500}>
+						<hr />
+						<Skills />
+					</Slide>
+				</Container>
+			</div>
+		</div>
 	);
 };
 
